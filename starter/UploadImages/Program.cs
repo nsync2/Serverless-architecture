59c56913f5c7f989d774dda7faada47fec65b1d6
Nagen Singh <nagsingh@deloitte.com>
2020-05-17 03:37:47 -0500

    Finished the ExportLicensePlates function

--- starter/UploadImages/Program.cs
+++ starter/UploadImages/Program.cs
@@ -33,6 +33,19 @@ namespace UploadImages
             }
 
             int choice = 1;
+            String strHostName = string.Empty;
+            // Getting Ip address of local machine...
+            // First get the host name of local machine.
+            strHostName = Dns.GetHostName();
+            Console.WriteLine("Local Machine's Host Name: " + strHostName);
+            // Then using host name, get the IP address list..
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+
+            for (int i = 0; i < addr.Length; i++)
+            {
+                Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString());
+            }
             Console.WriteLine("Enter one of the following numbers to indicate what type of image upload you want to perform:");
             Console.WriteLine("\t1 - Upload a handful of test photos");
             Console.WriteLine("\t2 - Upload 1000 photos to test processing at scale");
